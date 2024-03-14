b89ea1efae5bbaaa8328cde9639c63ebb0d7b6d0
Ihor Kytsak <kitsak735@gmail.com>
2024-03-14 21:08:16 +0200

    Created layer based generation NFT script

---
+++ layers/content.ts
@@ -0,0 +1,221 @@
+export const content = {
+  metadataTemplate: (tokenId: number, attributes: any) => ({
+    name: `NFT #${tokenId}`,
+    description: `This is a description of the NFT #${tokenId}`,
+    // image: `https://ipfs.io/ipfs/QmdkW174m8zE31gV8t2zN5K9j71f7y6111234567890?filename=nft-${tokenId}.png`,
+    image: `imageURL`,
+    attributes: attributes,
+  }),
+  layers: [
+    {
+      name: 'background',
+      probability: 1.0,
+      options: [
+        {
+          name: 'light-blue',
+          file: 'light-blue.png',
+          weight: 1,
+        },
+        {
+          name: 'blue-gradient',
+          file: 'blue-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'green',
+          file: 'green.png',
+          weight: 1,
+        },
+        {
+          name: 'green-gradient',
+          file: 'green-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'light-green-gradient',
+          file: 'light-green-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'light-blue-gradient',
+          file: 'light-blue-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'light-pink',
+          file: 'light-pink.png',
+          weight: 1,
+        },
+        {
+          name: 'pink-gradient',
+          file: 'pink-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'light-pink-gradient',
+          file: 'light-pink-gradient.png',
+          weight: 1,
+        },
+        {
+          name: 'multi-color-grandient',
+          file: 'multi-color-grandient.png',
+          weight: 0.25,
+        },
+        {
+          name: 'yellow',
+          file: 'yellow.png',
+          weight: 0.5,
+        },
+      ],
+    },
+    {
+      name: 'body',
+      probability: 1.0,
+      options: [
+        {
+          name: 'body',
+          file: 'body.png',
+          weight: 1,
+        },
+      ],
+    },
+    {
+      name: 'bulb',
+      probability: 1.0,
+      options: [
+        {
+          name: 'blank',
+          file: 'blank.png',
+          weight: 1,
+        },
+        {
+          name: 'blue',
+          file: 'blue.png',
+          weight: 1,
+        },
+        {
+          name: 'normal',
+          file: 'normal.png',
+          weight: 1,
+        },
+        {
+          name: 'orange',
+          file: 'orange.png',
+          weight: 1,
+        },
+        {
+          name: 'purple',
+          file: 'purple.png',
+          weight: 0.25,
+        },
+        {
+          name: 'yellow',
+          file: 'yellow.png',
+          weight: 0.5,
+        },
+      ],
+    },
+    {
+      name: 'middle-color',
+      probability: 1.0,
+      options: [
+        {
+          name: 'color',
+          file: 'color.png',
+          weight: 1,
+        },
+        {
+          name: 'cyan',
+          file: 'cyan.png',
+          weight: 1,
+        },
+        {
+          name: 'light-blue',
+          file: 'light-blue.png',
+          weight: 1,
+        },
+        {
+          name: 'light-orange',
+          file: 'light-orange.png',
+          weight: 1,
+        },
+        {
+          name: 'light-rose',
+          file: 'light-rose.png',
+          weight: 0.25,
+        },
+        {
+          name: 'light-yellow',
+          file: 'light-yellow.png',
+          weight: 0.5,
+        },
+        {
+          name: 'orange',
+          file: 'orange.png',
+          weight: 0.5,
+        },
+        {
+          name: 'purple',
+          file: 'purple.png',
+          weight: 0.5,
+        },
+        {
+          name: 'red',
+          file: 'red.png',
+          weight: 0.5,
+        },
+        {
+          name: 'rose',
+          file: 'rose.png',
+          weight: 0.5,
+        },
+        {
+          name: 'yellow',
+          file: 'yellow.png',
+          weight: 0.5,
+        },
+      ],
+    },
+    {
+      name: 'question',
+      probability: 1.0,
+      options: [
+        {
+          name: 'blank',
+          file: 'blank.png',
+          weight: 1,
+        },
+        {
+          name: 'blue',
+          file: 'blue.png',
+          weight: 1,
+        },
+        {
+          name: 'creem',
+          file: 'creem.png',
+          weight: 1,
+        },
+        {
+          name: 'greeny-yellow',
+          file: 'greeny-yellow.png',
+          weight: 1,
+        },
+        {
+          name: 'normal',
+          file: 'normal.png',
+          weight: 1,
+        },
+        {
+          name: 'pink',
+          file: 'pink.png',
+          weight: 1,
+        },
+        {
+          name: 'red',
+          file: 'red.png',
+          weight: 1,
+        },
+      ],
+    },
+  ],
+}
---
+++ scripts/generateNFT.ts
@@ -0,0 +1,119 @@
+import mergeImages from 'merge-images'
+import { Canvas, Image } from 'node-canvas'
+import { MersenneTwister19937, bool, real } from 'random-js'
+import path from 'path'
+import fs from 'fs'
+
+import { content } from '../layers/content'
+
+const layersPath = path.join(process.cwd(), 'layers')
+
+function saveBase64Image(base64PngImage: string, fileName: string) {
+  const base64 = base64PngImage.split(',')[1]
+  const imageBuffer = Buffer.from(base64, 'base64')
+
+  fs.writeFileSync(fileName, imageBuffer)
+}
+
+async function margeLayersAndSave(layers: string[], outputFile: string) {
+  const image = await mergeImages(layers, { Canvas: Canvas, Image: Image })
+  saveBase64Image(image, outputFile)
+}
+
+type IOption = {
+  name: string
+  file: string
+  weight: number
+}
+
+type ILayer = {
+  name: string
+  probability: number
+  options: IOption[]
+}
+
+function randomlySelectedLayers(layersPath: string, layers: Array<ILayer>) {
+  const mt = MersenneTwister19937.autoSeed()
+
+  let images = []
+  let selectedTraits: { [key: string]: string } = {}
+
+  for (const layer of layers) {
+    if (bool(layer.probability)(mt)) {
+      let selected = pickWeighted(mt, layer.options)
+      selectedTraits[layer.name] = selected.name
+      images.push(path.join(layersPath, layer.name, selected.file))
+    }
+  }
+
+  return { images, selectedTraits }
+}
+
+function pickWeighted(mt: MersenneTwister19937, options: IOption[]): IOption {
+  const weightSum = options.reduce((acc, option) => {
+    return acc + (option.weight ?? 1.0)
+  }, 0)
+
+  const r = real(0.0, weightSum, false)(mt)
+
+  let summedWeight = 0.0
+  let choosenOption = options[0]
+  for (const option of options) {
+    summedWeight += option.weight ?? 1.0
+    if (r < summedWeight) {
+      choosenOption = option
+      return option
+    }
+  }
+  return choosenOption
+}
+
+function generateMetadata(content: any, tokenId: number, traits: any) {
+  let attributes: any = []
+  for (const [trait_type, value] of Object.entries(traits)) {
+    attributes.push({
+      trait_type,
+      value,
+    })
+  }
+  return content.metadataTemplate(tokenId, attributes)
+}
+
+async function generateNFT(
+  num: number,
+  layersPath: string,
+  outputPath: string
+) {
+  let generated = new Set()
+
+  for (let tokenId = 0; tokenId < num; tokenId++) {
+    console.log(`Generating NFT #${tokenId} ...`)
+    let selection = randomlySelectedLayers(layersPath, content.layers)
+    const traits = JSON.stringify(selection.selectedTraits)
+
+    if (generated.has(traits)) {
+      console.log('Duplicate detected. Retrying...')
+      tokenId--
+      continue
+    } else {
+      generated.add(traits)
+      await margeLayersAndSave(
+        selection.images,
+        path.join(outputPath, `${tokenId}.png`)
+      )
+
+      let metadata = generateMetadata(
+        content,
+        tokenId,
+        selection.selectedTraits
+      )
+      fs.writeFileSync(
+        path.join(outputPath, `${tokenId}`),
+        JSON.stringify(metadata, null, 2)
+      )
+    }
+  }
+}
+
+const outputPath = path.join(process.cwd(), 'generated-images')
+generateNFT(5, layersPath, outputPath)
